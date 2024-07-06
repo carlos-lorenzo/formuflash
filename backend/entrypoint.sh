33696cf5827a72c891ef0f76afd58ffa1eff2416
Carlos Lorenzo <clorenzozuniga@gmail.com>
2024-07-06 12:29:24 +0200

    - updated gunicorn config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate --run-syncdb --no-input
 python manage.py collectstatic --no-input
 
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+gunicorn --config gunicorn_config.py.ini backend.wsgi:application
