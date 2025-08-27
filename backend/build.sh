d474a0e4788a27a1665d934a6f631f9458d6532f
Carlos Lorenzo <clorenzozuniga@gmail.com>
2025-08-27 12:17:37 +0200

    fixed typo

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-pytho3.12 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
