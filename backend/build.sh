6f396ba2d7b799b9df9658433846314fb0faf66f
Carlos Lorenzo <clorenzozuniga@gmail.com>
2025-08-27 12:16:42 +0200

    updated again

--- backend/build.sh
+++ backend/build.sh
@@ -2,7 +2,7 @@
 
 # Build the project
 echo "Building the project..."
-pytho3.9 -m pip install -r requirements.txt
+pytho3.12 -m pip install -r requirements.txt
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
